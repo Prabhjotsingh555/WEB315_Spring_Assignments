2f9fde9920bc042d5d6c3eabc423bd9c8a8cd1c0
Prabhjot singh <prabhjot.singh7131@icloud.com>
2023-06-23 17:55:33 -0400

    Added new properties

--- cars/Pages/Models/cars1.cs
+++ cars/Pages/Models/cars1.cs
@@ -8,16 +8,14 @@ namespace cars.Models
         public int ID { get; set; }
         [DataType(DataType.Date)]
         public DateTime BuildYear { get; set; }
+        public decimal Price { get; set; }
+        public int Rating { get; set;}
         public string Name { get; set; }
         public string Company { get; set; }
         public string Engine { get; set; }
         public string Country { get; set; }
         public string Features { get; set; }
-        public decimal Price { get; set; }
-
-
-
-        /*public int Rating { get; set;}
-        public string RelatedFilms {get; set;}*/
+        
+        public string Colour {get; set;}
     }
 }
