e7fac86a09c51f96335b295de320c9505c050bb2
Prabhjot singh <prabhjot.singh7131@icloud.com>
2023-06-23 18:30:12 -0400

    Added validation

--- cars/Pages/Models/cars1.cs
+++ cars/Pages/Models/cars1.cs
@@ -7,17 +7,46 @@ namespace cars.Models
     public class cars1
     {
         public int ID { get; set; }
+        
         [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
         public DateTime BuildYear { get; set; }
+
+        [Range(1, 100000)]
+        [DataType(DataType.Currency)]
         [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
-        public int Rating { get; set;}
+
+        [RegularExpression(@"^.{1,2}$", ErrorMessage = "Rating must be 1 or 2 characters long.")]
+        [Required]
+        public int Rating { get; set; }
+
+        [StringLength(60, MinimumLength = 4)]
+        [Required]
         public string Name { get; set; }
+
+        [RegularExpression(@"^[a-zA-Z\s]*$", ErrorMessage = "Company must contain only letters and white spaces.")]
+        [Required]
+        [StringLength(30)]
         public string Company { get; set; }
+        
+        [RegularExpression(@"^[a-zA-Z\s]*$", ErrorMessage = "Engine must contain only letters and white spaces.")]
+        [Required]
+        [StringLength(30)]
         public string Engine { get; set; }
+
+        [RegularExpression(@"^[a-zA-Z\s]*$", ErrorMessage = "Country must contain only letters and white spaces.")]
+        [Required]
+        [StringLength(30)]
         public string Country { get; set; }
-        public string Features { get; set; }       
-        public string Colour {get; set;}
+
+        [RegularExpression(@"^[a-zA-Z\s]*$", ErrorMessage = "Features must contain only letters and white spaces.")]
+        [Required]
+        [StringLength(30)]
+        public string Features { get; set; }   
+
+        [RegularExpression(@"^[A-Z][a-zA-Z\s]*$", ErrorMessage = "Colour must start with an uppercase letter and contain only letters and white spaces.")]
+        [Required]
+        public string Colour { get; set; }
     }
 }
